42e7982271091eb0347986f3a2b7123bddb31dd2
fernandaoliveiracasadiconti <fernanda.oliveira@casadiconti.com.br>
2022-05-06 18:17:48 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:'*'}));
 // app.use(express.json());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
