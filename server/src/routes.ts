1be00d4e286f229d93a0f47e003e1aad8797e238
fernandaoliveiracasadiconti <fernanda.oliveira@casadiconti.com.br>
2022-05-06 18:11:36 -0300

    teste

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,7 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
    const { type, comment, screenshot } = req.body;
 
-   try {       
+       
        const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
        const nodemailerMailAdapter = new NodemailerMailAdapter();    
     
@@ -25,9 +25,5 @@ routes.post('/feedbacks', async (req, res) => {
         });    
     
         return res.status(201).send();
-   } catch (error) {
-    //    console.error(error);
-
-       return res.status(500).send();       
-   }
+ 
 })
